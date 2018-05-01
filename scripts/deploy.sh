3a61987d99234e608f838763829190859b9c2f3c
Kenneth Sejrsgaard-Jacobsen <kse@flexdanmark.dk>
2018-05-01 18:10:21 +0200

    Wrong place for dist directory

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 LAKSEBOT_HOME=/home/travis/build/ksj2209/laksebot
-ROOT=`pwd`
 cd ${LAKSEBOT_HOME}/src/LakseBot/bin/Release/netcoreapp2.1/ubuntu-x64/publish
 chmod a+x LakseBot.dll
 
-if ! [[ -d dist ]]; then
-    mkdir -p dist
+if ! [[ -d ${LAKSEBOT_HOME}/dist ]]; then
+    mkdir -p ${LAKSEBOT_HOME}/dist
 fi
 
 zip -r ${ROOT}/dist/laksebot.zip *
-sshpass -p $FTP_PWD scp -o StrictHostKeyChecking=no -r ${ROOT}/dist/laksebot.zip $FTP_USER@$FTP_HOST:$FTP_DIR
+sshpass -p $FTP_PWD scp -o StrictHostKeyChecking=no -r ${LAKSEBOT_HOME}/dist/laksebot.zip $FTP_USER@$FTP_HOST:$FTP_DIR
 sshpass -p $FTP_PWD ssh -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST './deploy'
