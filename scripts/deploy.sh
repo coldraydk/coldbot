37684f99b7b97e396cd8278cd93ae03a3661962f
Kenneth Sejrsgaard-Jacobsen <kse@flexdanmark.dk>
2018-05-01 18:10:55 +0200

    Forgot one deprecated variable

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,6 @@ if ! [[ -d ${LAKSEBOT_HOME}/dist ]]; then
     mkdir -p ${LAKSEBOT_HOME}/dist
 fi
 
-zip -r ${ROOT}/dist/laksebot.zip *
+zip -r ${LAKSEBOT_HOME}/dist/laksebot.zip *
 sshpass -p $FTP_PWD scp -o StrictHostKeyChecking=no -r ${LAKSEBOT_HOME}/dist/laksebot.zip $FTP_USER@$FTP_HOST:$FTP_DIR
 sshpass -p $FTP_PWD ssh -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST './deploy'
