e4ba462247654f3a489bc8c35c68512974b33079
Kenneth Sejrsgaard-Jacobsen <kse@flexdanmark.dk>
2018-05-01 17:49:14 +0200

    Create dist if it does not exist

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
 LAKSEBOT_HOME=/home/travis/build/ksj2209/laksebot
 ROOT=`pwd`
 cd ${LAKSEBOT_HOME}/src/LakseBot/bin/Release/netcoreapp2.1/ubuntu-x64/publish
 chmod a+x LakseBot.dll
+
+if ! [[ -d dist ]]; then
+    mkdir -p dist
+fi
+
 zip -r ${ROOT}/dist/laksebot.zip *
 sshpass -p $FTP_PWD scp -o StrictHostKeyChecking=no -r ${ROOT}/dist/laksebot.zip $FTP_USER@$FTP_HOST:$FTP_DIR
 sshpass -p $FTP_PWD ssh -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST './deploy'
