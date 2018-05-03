e5943892e4ead38de0f7c91b268255d4230ad008
Mathies Grøndahl Kristensen <mgk@flexdanmark.dk>
2018-05-03 12:14:38 +0200

    Even more clean up

--- src/LakseBot/deploy.sh
+++
@@ -1,6 +0,0 @@
-rm -rf ./bin/Release
-dotnet publish -c Release -r ubuntu-x64
-tar -zcvf laksebot.tar.gz -C ./bin/Release/netcoreapp2.1/ubuntu-x64/publish .
-scp laksebot.tar.gz root@sejrsgaard-jacobsen.dk:/tmp/
-ssh root@sejrsgaard-jacobsen.dk '/opt/deploy-laksebot.sh'
-rm laksebot.tar.gz
